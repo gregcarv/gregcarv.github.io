1caa0a14437f145e2963c1fde2673f5aa6f63665
Greg Carvalhal <gregory.carvalhal@caribmedia.com>
2019-07-05 12:35:56 -0400

    added aw-suntimes demo

---
+++ aw-suntimes-demo/precache-manifest.740dcdd9151b6f8ebab4bc8fbe9dab91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1596b7fa360352f661b15da3f7b539",
+    "url": "/aw-suntimes-demo/index.html"
+  },
+  {
+    "revision": "5ae8fef5447cd27d6867",
+    "url": "/aw-suntimes-demo/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "c869de296445661974ef",
+    "url": "/aw-suntimes-demo/static/css/main.3aa850ca.chunk.css"
+  },
+  {
+    "revision": "5ae8fef5447cd27d6867",
+    "url": "/aw-suntimes-demo/static/js/2.d0d19931.chunk.js"
+  },
+  {
+    "revision": "c869de296445661974ef",
+    "url": "/aw-suntimes-demo/static/js/main.4e1561dd.chunk.js"
+  },
+  {
+    "revision": "6c00d6aae56c7e01bfce",
+    "url": "/aw-suntimes-demo/static/js/runtime~main.7cba42ca.js"
+  }
+]);

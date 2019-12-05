4a1262c8db89b8e18b46f00f79719f6fa93f690e
Gregory Carvalhal <gregory.carvalhal@caribmedia.com>
2019-12-05 13:50:35 +0100

    updated cv

--- precache-manifest.34b7605f98d1be2b745baef1a4d5e459.js
+++ precache-manifest.34b7605f98d1be2b745baef1a4d5e459.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f558b000bfeb6134034ddf559bf43132",
+    "revision": "d8c821b605c8035e637c546331b6c7af",
     "url": "/index.html"
   },
   {
-    "revision": "59a1bd37e114a1560448",
+    "revision": "94f64a69f86195a62264",
     "url": "/static/css/main.dc512ae1.chunk.css"
   },
   {
-    "revision": "2e2b7e8afb279efdda05",
-    "url": "/static/js/2.36902f9f.chunk.js"
+    "revision": "2ca042216d16a939dd2d",
+    "url": "/static/js/2.184faea8.chunk.js"
   },
   {
-    "revision": "59a1bd37e114a1560448",
-    "url": "/static/js/main.0130bfd2.chunk.js"
+    "revision": "94f64a69f86195a62264",
+    "url": "/static/js/main.afd79113.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -22,6 +22,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f7fd24d36a91343010e6ff9f0c9bf08c",
     "url": "/static/media/Greg_Carvalhal_CV_Frontend_Developer_2019.f7fd24d3.pdf"
+  },
+  {
+    "revision": "3fc5d274e5ba88edb136082b1a3e56cf",
+    "url": "/static/media/Gregory_Carvalhal_Recommendation_CaribMedia_2019.3fc5d274.pdf"
   },
   {
     "revision": "9c941a2495e5375e4b03aeca99429cdc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2517c1f72d6c66a7078c27058ac434b1.js"
+  "/precache-manifest.34b7605f98d1be2b745baef1a4d5e459.js"
 );
 
 self.addEventListener('message', (event) => {

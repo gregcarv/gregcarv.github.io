ab48accc6201f32ab78f77fa521fadb168b148aa
Gregory Carvalhal <gregcarv@gmail.com>
2023-01-08 20:43:12 +0100

    latest build

---
+++ precache-manifest.a4c961873ad8af50004468aedc7844aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fc48dafb8c61790422e",
+    "url": "/static/css/main.656d72b8.chunk.css"
+  },
+  {
+    "revision": "1fc48dafb8c61790422e",
+    "url": "/static/js/main.4f852a4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38989875c3add396fc32",
+    "url": "/static/js/2.d97ac031.chunk.js"
+  },
+  {
+    "revision": "2ecf42acd11a7a4107f8c52a1c767b92",
+    "url": "/static/media/fabafLogo.2ecf42ac.png"
+  },
+  {
+    "revision": "a30def1b46716a6f9a26631a6d55658a",
+    "url": "/static/media/Greg_Carvalhal_CV_Frontend_Developer_2023.a30def1b.pdf"
+  },
+  {
+    "revision": "3fc5d274e5ba88edb136082b1a3e56cf",
+    "url": "/static/media/Gregory_Carvalhal_Recommendation_CaribMedia_2019.3fc5d274.pdf"
+  },
+  {
+    "revision": "c6a27750a990951fd98667f9f4d06574",
+    "url": "/index.html"
+  }
+];
